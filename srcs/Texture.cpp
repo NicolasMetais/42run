892e9c0b5fa64e8df0a5fc36ff77e251518affba
Nicolas Metais <nico.metais@hotmail.fr>
2026-04-20 17:00:48 +0200

    png fixed

--- srcs/Texture.cpp
+++ srcs/Texture.cpp
@@ -15,6 +15,8 @@ void Texture::loadTexture(const std::string& path) {
 void Texture::loadPNG(const std::string& path) {
     FILE* fp = fopen(path.c_str(), "rb");
     if (!fp) throw std::runtime_error("Cannot open PNG file");
+
+	//signature check
     png_byte header[8];
     fread(header, 1, 8, fp);
     if (png_sig_cmp(header, 0, 8)) {
@@ -26,7 +28,10 @@ void Texture::loadPNG(const std::string& path) {
     if (!png_ptr) { fclose(fp); throw std::runtime_error("png_create_read_struct failed"); }
 
     png_infop info_ptr = png_create_info_struct(png_ptr);
-    if (!info_ptr) { png_destroy_read_struct(&png_ptr, nullptr, nullptr); fclose(fp); throw std::runtime_error("png_create_info_struct failed"); }
+    if (!info_ptr) { png_destroy_read_struct(&png_ptr, nullptr, nullptr);
+		fclose(fp); 
+		throw std::runtime_error("png_create_info_struct failed");
+	}
 
     if (setjmp(png_jmpbuf(png_ptr))) {
         png_destroy_read_struct(&png_ptr, &info_ptr, nullptr);
@@ -38,24 +43,20 @@ void Texture::loadPNG(const std::string& path) {
     png_set_sig_bytes(png_ptr, 8);
     png_read_info(png_ptr, info_ptr);
 
-    width = png_get_image_width(png_ptr, info_ptr);
-    height = png_get_image_height(png_ptr, info_ptr);
-    png_byte color_type = png_get_color_type(png_ptr, info_ptr);
-    png_byte bit_depth = png_get_bit_depth(png_ptr, info_ptr);
-
-    // Conversion en RGBA 8 bits
-    if (bit_depth == 16) png_set_strip_16(png_ptr);
-    if (color_type == PNG_COLOR_TYPE_PALETTE) png_set_palette_to_rgb(png_ptr);
-    if (color_type == PNG_COLOR_TYPE_GRAY && bit_depth < 8) png_set_expand_gray_1_2_4_to_8(png_ptr);
-    if (png_get_valid(png_ptr,  info_ptr, PNG_INFO_tRNS)) png_set_tRNS_to_alpha(png_ptr);
-    if (color_type == PNG_COLOR_TYPE_RGB || color_type == PNG_COLOR_TYPE_GRAY) png_set_filler(png_ptr, 0xFF, PNG_FILLER_AFTER);
-    if (color_type == PNG_COLOR_TYPE_GRAY || color_type == PNG_COLOR_TYPE_GRAY_ALPHA) png_set_gray_to_rgb(png_ptr);
+	png_set_expand(png_ptr);
+	png_set_strip_16(png_ptr);
+	png_set_gray_to_rgb(png_ptr);
+	png_set_add_alpha(png_ptr, 0xFF, PNG_FILLER_AFTER); 
 
     png_read_update_info(png_ptr, info_ptr);
 
+    width = png_get_image_width(png_ptr, info_ptr);
+    height = png_get_image_height(png_ptr, info_ptr);
     bpp = 4;
     inputFormat = GL_RGBA;
+
 	png_size_t rowbytes = png_get_rowbytes(png_ptr, info_ptr);
+	
     data.resize(rowbytes * height);
     std::vector<png_bytep> row_pointers(height);
     for (int y = 0; y < (int)height; y++)
