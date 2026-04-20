0c60f8ceb86d79546b77ec3fcd84377aff731c7f
Nicolas Metais <nico.metais@hotmail.fr>
2026-04-20 16:50:09 +0200

    removed shitty and useless compilation file

--- obj/srcs/Shader.d
+++
@@ -1,8 +0,0 @@
-obj/srcs/Shader.o: srcs/Shader.cpp includes/Shader.hpp \
-  loader/include/glad/glad.h loader/include/KHR/khrplatform.h
-
-includes/Shader.hpp:
-
-loader/include/glad/glad.h:
-
-loader/include/KHR/khrplatform.h:
--- obj/srcs/Window.d
+++
@@ -1,9 +0,0 @@
-obj/srcs/Window.o: srcs/Window.cpp includes/Window.hpp \
-  includes/../loader/include/glad/glad.h \
-  loader/include/KHR/khrplatform.h
-
-includes/Window.hpp:
-
-includes/../loader/include/glad/glad.h:
-
-loader/include/KHR/khrplatform.h:
